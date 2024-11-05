1355ed7ebcd3d337bf2589567ab5739691232a06
natasha-lim <23014690@myrp.edu.sg>
2024-11-05 17:37:44 +0800

    Updated App.js

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, Alert, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+{/*Custom Components*/}
+const InputBox = ({label, onChangeText}) => {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText} />
+        </View>
+    );
+};
+
+const MyApp = () => {
+    // let username="";
+    // let password="";
+    const [username, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [type, setType] = useState('');
+
+    return (
+
+    //   <Text>Username:</Text>
+    // <TextInput style={{borderWidth: 1}} onChangeText={(text) => username = text}/>
+    //
+    //     <Text>Password:</Text>
+    //     <TextInput style={{borderWidth: 1}} onChangeText={(text) => password = text}/>
+    //
+    //     <TouchableOpacity onPress={()=>ToastAndroid.show("Hello" + username + " " + password, ToastAndroid.SHORT)}>
+    //         <Text>LOG IN</Text>
+    //     </TouchableOpacity>
+
+      <View style={{padding: 20, paddingTop: 50}}>
+
+          {/*Exercise 1B*/}
+          <Text>User Type:</Text>
+          <RNPickerSelect
+              onValueChange={(value) => setType(value)}
+              items={[
+                  { label: 'Admin', value: 'Admin' },
+                  { label: 'Guest', value: 'Guest' }
+              ]}
+          />
+
+          {/*/!*States Examples*!/*/}
+          {/*<Text>Password</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onChangeText={(text => setPassword(text))}*/}
+          {/*/>*/}
+
+          {/*<TouchableOpacity onPress={() => ToastAndroid.show(password, ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*<Text>{password}</Text>*/}
+
+          {/*Exercise 1A*/}
+          {/*<Text> Password: </Text>*/}
+          {/*<TextInput style={{borderWidth: 1}} />*/}
+
+          {/*/!*Exercise 1C and 1D*!/*/}
+          {/*<Button*/}
+          {/*    title="Log In"*/}
+          {/*    onPress={()=>Alert.alert("Log In")}*/}
+          {/*/>*/}
+
+          {/*Exercise 1E*/}
+          {/*<TouchableOpacity*/}
+          {/*    onPress={()=>Alert.alert("Welcome")}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*Exercise 1F*/}
+          {/*<TouchableOpacity*/}
+          {/*    onPress={()=>ToastAndroid.show("Welcome", ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN 2</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*Exercise 1G*/}
+          {/*<Text>Username:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onValueChange={(text) => setUser(text)}*/}
+          {/*/>*/}
+
+          {/*<Text>Password:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onChangeText={(text => setPassword(text))}*/}
+          {/*/>*/}
+
+          <InputBox label="User Name:" onChangeText={(text)=>setUser(text)} />
+          <InputBox label="Password:" onChangeText={(text)=>setPassword(text)} />
+
+          <TouchableOpacity onPress={() => ToastAndroid.show('Welcome' + type + ' ' + username, ToastAndroid.SHORT)}>
+              <Text>LOG IN</Text>
+          </TouchableOpacity>
+
+          <Text>{password}</Text>
+
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyApp;
